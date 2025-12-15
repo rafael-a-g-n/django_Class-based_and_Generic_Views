f868cb986c0929a97290ca6146304ef5bae177e8
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-12-15 16:14:13 +0000

    Update requirements and implement course views and URLs

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -39,3 +39,38 @@ from django.http import Http404
 #       course.total_enrollment += 1
 #       course.save()
 #       return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
+
+# Note that we are subclassing CourseListView from base View class
+class CourseListView(View):
+
+    # Handles get request
+    def get(self, request):
+        context = {}
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context)
+
+class EnrollView(View):
+
+    # Handles post request
+    def post(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase total enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
+class CourseDetailsView(View):
+
+    # Handles get request
+    def get(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        context = {}
+        try:
+            course = Course.objects.get(pk=course_id)
+            context['course'] = course
+            return render(request, 'onlinecourse/course_detail.html', context)
+        except Course.DoesNotExist:
+            raise Http404("No course matches the given id.")
